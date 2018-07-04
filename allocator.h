faaebec9e4fc2ce95d013b018c7f0b1a4516cb67
snake8 <spoodiman4@gmail.com>
2018-07-05 00:47:14 +0300

    Update allocator.h

--- allocator.h
+++ allocator.h
@@ -1,11 +1,6 @@
 #if !defined(MALLOC_H)
-/* ========================================================================
-   $File: $
-   $Date: $
-   $Revision: $
-   $Creator: Daniel Solovich $
-   $Notice: (C) Copyright 2016 by Solovich, Inc. All Rights Reserved. $
-   ======================================================================== */
+
+#include "shared_includes.h"
 
 #define Align4(x) (((((x) -1)>>2)<<2)+4)
 #define internal static
@@ -13,24 +8,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+
 typedef unsigned int u32;
 typedef bool b8; 
+typedef size_t memory_index;
 
 struct block
 {
-    u32 Size;
+    memory_index Size;
     block *Next;
-    block *Prev;
     void *Ptr; // NOTE(dan): Pointer to the allocated block. 
-    b8 Free;
+    b32 Free;
     char Data[1]; 
 }; 
 
-#define BLOCK_SIZE sizeof(block)
+#define BLOCK_SIZE 20
 
-void *Allocate(u32 Size);
+void *Allocate(memory_index Size);
 void Deallocate(void *Ptr);
-void *Reallocate(void *Ptr, u32 Size);
+void *Reallocate(void *Ptr, memory_index Size);
 
 #define MALLOC_H
 #endif
